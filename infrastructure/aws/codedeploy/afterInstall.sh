a0551fe830fde42e4da7ec32dc2df7ae496fb497
Ke <yuan.ke@northeastern.edu>
2019-11-05 21:56:11 -0800

    circleci

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd /home/centos/webapp
+sudo rm -rf logs/*
 sudo chown -R centos:centos /home/centos/webapp
 sudo chmod +x demo-0.0.1-SNAPSHOT.jar
 #source /etc/profile.d/envvariable.sh
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/webapp/cloudwatch-config.json -s
-kill -9 $(ps -ef|grep webApp | grep -v grep | awk '{print$2}')
+kill -9 $(ps -ef|grep webApp | grep -v grep)
 nohup java -jar demo-0.0.1-SNAPSHOT.jar
