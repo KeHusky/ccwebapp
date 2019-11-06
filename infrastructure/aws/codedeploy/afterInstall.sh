5d3c7016a33f0765e28c1d2ea26f6e2d7a7d671b
Ke <yuan.ke@northeastern.edu>
2019-11-06 11:38:32 -0800

    fix

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -6,4 +6,4 @@ sudo chmod +x demo-0.0.1-SNAPSHOT.jar
 #source /etc/profile.d/envvariable.sh
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/webapp/cloudwatch-config.json -s
 kill -9 $(ps -ef|grep demo | grep -v grep)
-nohup java -jar demo-0.0.1-SNAPSHOT.jar
+java -jar demo-0.0.1-SNAPSHOT.jar
